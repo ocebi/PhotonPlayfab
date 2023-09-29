c5681923c22795f9bfed031ef4422997587d603b
Özgür Çebi <cebiozgur@outlook.com>
2023-09-29 15:24:48 +0300

    Add state machine system

---
+++ Assets/_Game/Scripts/State System/State.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateSystem
+{
+    public abstract class State : MonoBehaviour
+    {
+        protected StateMachine StateMachine;
+        protected GameObject Owner;
+        protected float StateStartTime;
+
+        public virtual void Initialize(StateMachine i_StateMachine, GameObject i_Owner)
+        {
+            StateMachine = i_StateMachine;
+            Owner = i_Owner;
+        }
+
+        public virtual void OnStateEnter()
+        {
+            StateStartTime = Time.time;
+        }
+
+        public virtual void OnStateUpdate()
+        {
+
+        }
+
+        public virtual void OnStateExit()
+        {
+
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/State System/StateMachine.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace StateSystem
+{
+    public class StateMachine : MonoBehaviour
+    {
+        public Action<string> OnStateChanged;
+        public bool ActivateCurrentStateGameObject;
+        public bool AutoStartStateMachine = true;
+        
+        public State CurrentState { get; private set; }
+        public bool IsInitialized => m_AvailableStatesInitialized;
+
+        [SerializeField, ReadOnly] 
+        private StateDictionary m_AvailableStates = new StateDictionary();
+        private bool m_AvailableStatesInitialized = false;
+        private bool m_IsStateMachinePaused = true;
+        
+        [ReadOnly]
+        public string CurrentStateName;
+
+        private void Awake()
+        {
+            var statesEnumerator = m_AvailableStates.GetEnumerator();
+            while (statesEnumerator.MoveNext())
+            {
+                statesEnumerator.Current.Value.Initialize(this, gameObject);
+            }
+            statesEnumerator.Dispose();
+            
+            m_AvailableStatesInitialized = true;
+            if (AutoStartStateMachine)
+                m_IsStateMachinePaused = false;
+        }
+
+        private void Update()
+        {
+            if (m_IsStateMachinePaused)
+                return;
+            if (!m_AvailableStatesInitialized) 
+                return;
+            if (CurrentState == null)
+            {
+                SetNewState(GetDefaultState());
+            }
+
+            CurrentState.OnStateUpdate();
+        }
+        
+        // private void SetStates()
+        // {
+        //     m_AvailableStates.Clear();
+        //     for (int i = 0; i < transform.childCount; i++)
+        //     {
+        //         var type = transform.GetChild(i).GetComponent<State>().GetType().ToString();
+        //         var state = transform.GetChild(i).GetComponent<State>();
+        //         if (!m_AvailableStates.ContainsKey(type))
+        //             m_AvailableStates.Add(type, state);
+        //         if (ActivateCurrentStateGameObject)
+        //             state.gameObject.SetActive(false);
+        //     }
+        // }
+
+        public void ActivateStateMachine()
+        {
+            m_AvailableStatesInitialized = true;
+            m_IsStateMachinePaused = false;
+        }
+
+        public void ResetStateMachine()
+        {
+            m_AvailableStatesInitialized = false;
+            m_IsStateMachinePaused = true;
+            CurrentState = null;
+            CurrentStateName = "";
+        }
+        
+        public void SetNewState(string newState)
+        {
+            if (CurrentState != null)
+            {
+                CurrentState.OnStateExit();
+                if (ActivateCurrentStateGameObject)
+                    CurrentState.gameObject.SetActive(false);
+            }
+            if (m_AvailableStates.ContainsKey(newState))
+            {
+                CurrentState = m_AvailableStates[newState];
+                CurrentState.OnStateEnter();
+                if (ActivateCurrentStateGameObject)
+                    CurrentState.gameObject.SetActive(true);
+                CurrentStateName = newState.ToString();
+                OnStateChanged.InvokeSafe(newState);
+            }
+            else
+                Debug.LogError($"State {newState} does not exist in the dictionary");
+        }
+
+        public string GetDefaultState()
+        {
+            return m_AvailableStates.First().Key;
+        }
+
+        private void OnDestroy()
+        {
+            OnStateChanged = null;
+        }
+    }
+    
+    
+    [Serializable] public class StateDictionary : UnitySerializedDictionary<string, State> { }
+}
