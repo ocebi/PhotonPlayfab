e5cd3c77adb3e22cb141bd37f4ca1ba96b18532b
Özgür Çebi <cebiozgur@outlook.com>
2023-09-30 14:00:19 +0300

    Import dotween

--- Assets/_Game/Scripts/Utils/ExtensionMethods.cs
+++ Assets/_Game/Scripts/Utils/ExtensionMethods.cs
@@ -8,6 +8,18 @@ public static class ExtensionMethods
     /// <summary>
     /// Calls the delegate if it is not null
     /// </summary>
+    /// <param name="i_Action">The action to be called</param>
+    public static void InvokeSafe(this Action i_Action)
+    {
+        if (i_Action != null)
+        {
+            i_Action.Invoke();
+        }
+    }
+    
+    /// <summary>
+    /// Calls the delegate if it is not null
+    /// </summary>
     /// <param name="action">The action to be called</param>
     /// <param name="i_Object">The object passed</param>
     public static void InvokeSafe<T>(this Action<T> action, T value)
